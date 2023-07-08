ea85781f834d3dabc4d487ba8603a03979dce644
LongCao928 <17512055928@163.com>
2023-07-08 23:23:27 +0800

    feat:antd Typography

--- src/components/AntdComBasicUse.tsx
+++ src/components/AntdComBasicUse.tsx
@@ -1,4 +1,4 @@
-import { Button, Space } from 'antd'
+import { Button, Space, Typography } from 'antd'
 import {
   SearchOutlined,
   UsergroupAddOutlined,
@@ -15,6 +15,8 @@ const Iconfont = createFromIconfontCN({
   scriptUrl: '//at.alicdn.com/t/font_3387618_v2slvmt19r.js',
 })
 
+const { Title, Paragraph, Text, Link } = Typography
+
 export default function AntdComBasicUse() {
   const handleButonClick = () => {
     console.log('危险')
@@ -25,6 +27,7 @@ export default function AntdComBasicUse() {
         {`
           .compont-area {
             margin: 15px 0;
+            text-align: center;
           }
         `}
       </style>
@@ -53,6 +56,27 @@ export default function AntdComBasicUse() {
         <LoadingOutlined />
         <HeartTwoTone twoToneColor="#eb2f96" className="" />
         <Iconfont type="icon-youkachongzhi" />
+      </div>
+      <div>
+        <Typography>
+          <Title>标题1</Title>
+          <Paragraph>
+            段落内容1 &nbsp;
+            <Link href="https://ant.design" target="_blank">
+              Ant Design (Link)
+            </Link>
+          </Paragraph>
+          <Title level={2}>标题2</Title>
+          <Paragraph>
+            段落内容2
+            <Text code>code</Text>
+            <Text keyboard>keyboard</Text>
+            <Text delete>delete</Text>
+            <Text underline>underline</Text>
+            <Text disabled>disabled</Text>
+            <Text type="success">success</Text>
+          </Paragraph>
+        </Typography>
       </div>
     </>
   )
