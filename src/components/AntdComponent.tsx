6762d80720fe9094c99fbb69fa4335cfb423bba9
LongCao928 <17512055928@163.com>
2023-07-09 21:20:56 +0800

    feat:Layout

--- src/components/AntdComponent.tsx
+++ src/components/AntdComponent.tsx
@@ -1,5 +1,37 @@
-import { Col, Row } from 'antd'
+import { Space, Col, Row, Layout } from 'antd'
 import '@/styles/antd/antdComponent.less'
+
+const { Header, Footer, Sider, Content } = Layout
+
+const headerStyle: React.CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 64,
+  paddingInline: 50,
+  lineHeight: '64px',
+  backgroundColor: '#7dbcea',
+}
+
+const contentStyle: React.CSSProperties = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#108ee9',
+}
+
+const siderStyle: React.CSSProperties = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#3ba0e9',
+}
+
+const footerStyle: React.CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: '#7dbcea',
+}
 
 export function AntdComponent() {
   return (
@@ -22,6 +54,18 @@ export function AntdComponent() {
         <Col span={6}>col 6-3</Col>
         <Col span={6}>col 6-4</Col>
       </Row>
+      <div className="mg">
+        <Space direction="vertical" style={{ width: '100%' }} size={[0, 48]}>
+          <Layout>
+            <Sider style={siderStyle}>Sider</Sider>
+            <Layout>
+              <Header style={headerStyle}>Header</Header>
+              <Content style={contentStyle}>Content</Content>
+              <Footer style={footerStyle}>Footer</Footer>
+            </Layout>
+          </Layout>
+        </Space>
+      </div>
     </>
   )
 }
